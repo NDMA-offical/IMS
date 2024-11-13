a6aba26e2e262767e44128ed6abf994c1cef4f74
zhaider113 <zhaider113@gmail.com>
2024-11-13 13:26:40 +0500

    funding source Description from table removed, also view status updated

--- resources/views/fundingsources/show.blade.php
+++ resources/views/fundingsources/show.blade.php
@@ -35,17 +35,17 @@
                             <div class="table-responsive">
                                 <table class="table table-hover table-striped">
                                     <tr>
-                    <td class="fw-bold">{{ __('Source Name') }}</td>
-                    <td>{{ $fundingsource->source_name }}</td>
-                </tr>
-<tr>
-                    <td class="fw-bold">{{ __('Source Desp') }}</td>
-                    <td>{{ $fundingsource->source_desp }}</td>
-                </tr>
-<tr>
-                                <td class="fw-bold">{{ __('Source Status') }}</td>
-                                <td>{{ $fundingsource->source_status == 1 ? 'True' : 'False' }}</td>
-                              </tr>
+                                        <td class="fw-bold">{{ __('Source Name') }}</td>
+                                        <td>{{ $fundingsource->source_name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="fw-bold">{{ __('Source Desp') }}</td>
+                                        <td>{{ $fundingsource->source_desp }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="fw-bold">{{ __('Source Status') }}</td>
+                                        <td>{{ $fundingsource->source_status == 1 ? 'Active' : 'Inactive' }}</td>
+                                    </tr>
                                     <tr>
                                         <td class="fw-bold">{{ __('Created at') }}</td>
                                         <td>{{ $fundingsource->created_at->format('Y-m-d H:i:s') }}</td>
