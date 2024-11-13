9186c4e34b3b81a8653577f8aa1d08548bfa45bd
zhaider113 <zhaider113@gmail.com>
2024-11-13 16:30:52 +0500

    toggle theme comment

--- public/mazer/static/js/initTheme.js
+++ public/mazer/static/js/initTheme.js
@@ -1,5 +1,7 @@
 const body = document.body;
 const theme = localStorage.getItem('theme')
+console.log(theme, "theme")
+// const theme = 'light'
 
 if (theme) 
   document.documentElement.setAttribute('data-bs-theme', theme)
